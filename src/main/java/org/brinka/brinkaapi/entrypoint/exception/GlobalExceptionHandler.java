2a0ac9ebe1d5b4c0e60515473182c21fd33ccb62
Vini-Boas <vinicius.vboas1@gmail.com>
2026-08-08 12:26:22 -0300

    fix: adicionar mensagem por categoria violada

--- src/main/java/org/brinka/brinkaapi/entrypoint/exception/GlobalExceptionHandler.java
+++ src/main/java/org/brinka/brinkaapi/entrypoint/exception/GlobalExceptionHandler.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.stream.Collectors;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -48,9 +50,16 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        String message = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(new ErrorResponse(ex.getBindingResult().toString()));
+                .body(new ErrorResponse(message));
     }
 
     // ------- Spring Security -------
