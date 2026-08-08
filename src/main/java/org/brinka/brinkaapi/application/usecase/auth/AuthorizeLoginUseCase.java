8168640b62ad5a3e342a7cb7dcea1e64aa1668a3
Vini-Boas <vinicius.vboas1@gmail.com>
2026-08-08 13:19:29 -0300

    fix: add toDomain login

--- src/main/java/org/brinka/brinkaapi/application/usecase/auth/AuthorizeLoginUseCase.java
+++ src/main/java/org/brinka/brinkaapi/application/usecase/auth/AuthorizeLoginUseCase.java
@@ -3,6 +3,9 @@ package org.brinka.brinkaapi.application.usecase.auth;
 import lombok.RequiredArgsConstructor;
 import org.brinka.brinkaapi.application.annotation.UseCase;
 import org.brinka.brinkaapi.domain.model.User;
+import org.brinka.brinkaapi.infra.persistence.entity.UserEntity;
+import org.brinka.brinkaapi.infra.persistence.mapper.ProductMapper;
+import org.brinka.brinkaapi.infra.persistence.mapper.UserMapper;
 import org.brinka.brinkaapi.infra.security.TokenService;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -12,11 +15,14 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 public class AuthorizeLoginUseCase {
     private final AuthenticationManager authenticationManager;
     private final TokenService tokenService;
+    private final UserMapper mapper;
 
     public String execute(String login, String senha) {
         var usernamePassword = new UsernamePasswordAuthenticationToken(login, senha);
         var auth = authenticationManager.authenticate(usernamePassword);
 
-        return tokenService.generateToken((User) auth.getPrincipal());
+        var userEntity = (UserEntity) auth.getPrincipal();
+
+        return tokenService.generateToken(mapper.toDomain(userEntity));
     }
 }
