87d6e08b1150ed43502977e0612cc59317e9007c
Vini-Boas <vinicius.vboas1@gmail.com>
2026-08-09 15:14:59 -0300

    feat: adicionar rota para ver endereco

--- src/main/java/org/brinka/brinkaapi/entrypoint/controller/UserController.java
+++ src/main/java/org/brinka/brinkaapi/entrypoint/controller/UserController.java
@@ -4,13 +4,16 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.brinka.brinkaapi.application.usecase.user.AddCardUseCase;
 import org.brinka.brinkaapi.application.usecase.user.GetCardUseCase;
+import org.brinka.brinkaapi.application.usecase.user.GetEnderecoUseCase;
 import org.brinka.brinkaapi.application.usecase.user.UpdateCardUseCase;
 import org.brinka.brinkaapi.domain.model.Card;
 import org.brinka.brinkaapi.entrypoint.dto.request.AddCardRequest;
 import org.brinka.brinkaapi.entrypoint.dto.request.UpdateCardRequest;
 import org.brinka.brinkaapi.entrypoint.dto.response.CardResponse;
+import org.brinka.brinkaapi.entrypoint.dto.response.EnderecoResponse;
 import org.brinka.brinkaapi.entrypoint.mapper.CardRequestMapper;
 import org.brinka.brinkaapi.entrypoint.mapper.CardResponseMapper;
+import org.brinka.brinkaapi.entrypoint.mapper.EnderecoResponseMapper;
 import org.hibernate.sql.Update;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,30 +24,39 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/usuarios")
 public class UserController {
-    private final CardRequestMapper requestMapper;
-    private final CardResponseMapper responseMapper;
+    private final CardRequestMapper cardRequestMapper;
+    private final CardResponseMapper cardResponseMapper;
+    private final EnderecoResponseMapper enderecoResponseMapper;
     private final AddCardUseCase addCardUseCase;
     private final UpdateCardUseCase updateCardUseCase;
     private final GetCardUseCase getCardUseCase;
+    private final GetEnderecoUseCase getEnderecoUseCase;
 
     @GetMapping("/cartao")
     public ResponseEntity<CardResponse> getCard(Authentication authentication) {
         String email = authentication.getName();
 
-        return ResponseEntity.ok(responseMapper.toResponse(getCardUseCase.execute(email)));
+        return ResponseEntity.ok(cardResponseMapper.toResponse(getCardUseCase.execute(email)));
     }
 
     @PostMapping("/cartao")
     public ResponseEntity<CardResponse> addCard(@RequestBody @Valid AddCardRequest request, Authentication authentication) {
         String email = authentication.getName();
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(responseMapper.toResponse(addCardUseCase.execute(requestMapper.toInput(request), email)));
+        return ResponseEntity.status(HttpStatus.CREATED).body(cardResponseMapper.toResponse(addCardUseCase.execute(cardRequestMapper.toInput(request), email)));
     }
 
     @PatchMapping("/cartao")
     public ResponseEntity<CardResponse> updateCard(@RequestBody @Valid UpdateCardRequest request, Authentication authentication) {
         String email = authentication.getName();
 
-        return ResponseEntity.ok(responseMapper.toResponse(updateCardUseCase.execute(requestMapper.toInput(request), email)));
+        return ResponseEntity.ok(cardResponseMapper.toResponse(updateCardUseCase.execute(cardRequestMapper.toInput(request), email)));
+    }
+
+    @GetMapping("/endereco")
+    public ResponseEntity<EnderecoResponse> getEndereco(Authentication authentication) {
+        String email = authentication.getName();
+
+        return ResponseEntity.ok(enderecoResponseMapper.toResponse(getEnderecoUseCase.execute(email)));
     }
 }
