faf3ad3ee26b43d003cacecf9ba13c43bd3cd8a8
Vini-Boas <vinicius.vboas1@gmail.com>
2026-08-09 14:45:36 -0300

    fix: adicionar Optional para find card e user

--- src/main/java/org/brinka/brinkaapi/infra/security/UserDetailsServiceImpl.java
+++ src/main/java/org/brinka/brinkaapi/infra/security/UserDetailsServiceImpl.java
@@ -1,6 +1,7 @@
 package org.brinka.brinkaapi.infra.security;
 
 import lombok.RequiredArgsConstructor;
+import org.brinka.brinkaapi.domain.exception.UserNotFoundException;
 import org.brinka.brinkaapi.infra.persistence.entity.UserEntity;
 import org.brinka.brinkaapi.infra.persistence.repository.JpaUserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,10 +16,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        UserEntity user = userRepository.findByEmail(email);
-        if (user == null) {
-            throw new UsernameNotFoundException("Usuário não encontrado: " + email);
-        }
-        return user;
+        return userRepository.findByEmail(email)
+                .orElseThrow(UserNotFoundException::new);
     }
 }
