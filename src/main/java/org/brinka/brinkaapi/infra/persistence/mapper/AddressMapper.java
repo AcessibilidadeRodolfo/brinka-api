e15966a0eca30c8a0c540398fda39c9d55537dac
Vini-Boas <vinicius.vboas1@gmail.com>
2026-08-12 10:53:07 -0300

    fix: adicionar usermapper em enderecomapper

--- src/main/java/org/brinka/brinkaapi/infra/persistence/mapper/AddressMapper.java
+++ src/main/java/org/brinka/brinkaapi/infra/persistence/mapper/AddressMapper.java
@@ -4,7 +4,7 @@ import org.brinka.brinkaapi.domain.model.Address;
 import org.brinka.brinkaapi.infra.persistence.entity.AddressEntity;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = UserMapper.class)
 public interface AddressMapper {
     AddressEntity toEntity(Address domain);
     Address toDomain(AddressEntity entity);
