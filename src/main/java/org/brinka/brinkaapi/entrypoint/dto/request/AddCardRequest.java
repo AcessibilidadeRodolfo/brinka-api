297f969497447802446b87b42aa73b0af240d4c0
Vini-Boas <vinicius.vboas1@gmail.com>
2026-08-12 21:33:11 -0300

    fix: adicionar snake case integracao

--- src/main/java/org/brinka/brinkaapi/entrypoint/dto/request/AddCardRequest.java
+++ src/main/java/org/brinka/brinkaapi/entrypoint/dto/request/AddCardRequest.java
@@ -10,12 +10,12 @@ import java.time.LocalDate;
 
 public record AddCardRequest(
         @NotBlank(message = "obrigatório")
-        String numero_cartao,
+        String numeroCartao,
         @NotBlank(message = "obrigatório")
-        String nome_titular,
+        String nomeTitular,
         @NotNull(message = "obrigatório")
         @Future(message = "não pode ser expirado")
-        LocalDate data_validade,
+        LocalDate dataValidade,
         @NotBlank(message = "obrigatório")
         @Pattern(regexp = "^\\d{3}$", message = "deve conter exatamente três números")
         String cvc
