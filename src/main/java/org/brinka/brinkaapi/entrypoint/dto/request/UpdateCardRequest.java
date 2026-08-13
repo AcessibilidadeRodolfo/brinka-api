ae190a4d5f93cefdf82b3e69ca77b90ec0f9bbfb
Vini-Boas <vinicius.vboas1@gmail.com>
2026-08-12 21:33:11 -0300

    fix: adicionar snake case integracao

--- src/main/java/org/brinka/brinkaapi/entrypoint/dto/request/UpdateCardRequest.java
+++ src/main/java/org/brinka/brinkaapi/entrypoint/dto/request/UpdateCardRequest.java
@@ -6,10 +6,10 @@ import jakarta.validation.constraints.Pattern;
 import java.util.Date;
 
 public record UpdateCardRequest(
-        String numero_cartao,
-        String nome_titular,
+        String numeroCartao,
+        String nomeTitular,
         @Future(message = "não pode ser expirado")
-        Date data_validade,
+        Date dataValidade,
         @Pattern(regexp = "^\\d{3}$", message = "deve conter exatamente três números")
         String cvc
 ) {
